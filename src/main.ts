86e0e543a7a52865b04530de7cef0b1ce7cf4629
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-10-27 17:49:49 -0300

    institutopaliativo-02: remodelando o projeto

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,11 @@ import { createApp } from 'vue';
 import { createPinia } from 'pinia';
 
 import App from './App.vue';
-import router from './router';
+import loginRouter from './modules/login/router';
 
 const app = createApp(App);
 
 app.use(createPinia());
-app.use(router);
+app.use(loginRouter);
 
 app.mount('#app');
