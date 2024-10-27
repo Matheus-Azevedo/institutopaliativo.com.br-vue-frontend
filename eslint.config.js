6de6c3720c22fb2bda12d14e1f52574b030300ca
Matheus Eduardo <matheuseduardo.jp@gmail.com>
2024-10-27 00:33:16 -0300

    institutopaliativo-01: formatando o código

--- eslint.config.js
+++ eslint.config.js
@@ -1,25 +1,40 @@
-import pluginVue from 'eslint-plugin-vue'
-import vueTsEslintConfig from '@vue/eslint-config-typescript'
-import pluginVitest from '@vitest/eslint-plugin'
-import skipFormatting from '@vue/eslint-config-prettier/skip-formatting'
+import pluginVue from 'eslint-plugin-vue';
+import vueTsEslintConfig from '@vue/eslint-config-typescript';
+import pluginVitest from '@vitest/eslint-plugin';
+import skipFormatting from '@vue/eslint-config-prettier/skip-formatting';
 
 export default [
   {
+    // Configuração para os arquivos a serem lintados
     name: 'app/files-to-lint',
     files: ['**/*.{ts,mts,tsx,vue}'],
   },
 
   {
+    // Configuração para os arquivos a serem ignorados
     name: 'app/files-to-ignore',
     ignores: ['**/dist/**', '**/dist-ssr/**', '**/coverage/**'],
   },
 
+  // Configurações essenciais do ESLint para Vue
   ...pluginVue.configs['flat/essential'],
+
+  // Configurações do TypeScript para Vue
   ...vueTsEslintConfig(),
-  
+
+  // Configurações do Vitest
   {
     ...pluginVitest.configs.recommended,
     files: ['src/**/__tests__/*'],
   },
+
+  // Configurações de formatação
   skipFormatting,
-]
+
+  // Definição das regras
+  {
+    rules: {
+      semi: ['error', 'always'],
+    },
+  },
+];
